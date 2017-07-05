9dface6a17b99a372df08997107cc5d155671f7b
JimSick <877415861@qq.com>
2017-07-06 00:10:43 +0800

    django test

--- xxx/xxx/settings.py
+++ xxx/xxx/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '#5kh7ph55iu*41t_+u@fzprnmd80v*-lw7szn2ijg$in&oz2is'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
